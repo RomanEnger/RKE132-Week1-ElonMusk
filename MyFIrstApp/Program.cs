d08682869ec22b66695b5c0204f01e539269360a
RomanEnger <Roman.engertzisikov@gmail.com>
2024-02-12 13:30:40 +0200

    create new app

---
+++ MyFIrstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is Elon Musk
+
+//rakendus küsib kasutajal sisestada tema Net Worth
+//rakendus tervitab kasutajat Forbsi nimekirja järgi
+
+Console.WriteLine("Enter Your Name:"); //Output
+//string - s6ne
+string userName = Console.ReadLine();  //Input
+
+Console.WriteLine("Hello" + ", "+ userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");          //Output
